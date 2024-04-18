391a23daac7e785c94a9d4d3b1b84ac0499d4d8f
Raeshana <rsookhoo@andrew.cmu.edu>
2024-04-18 06:14:20 -0400

    Interactable spelling error
    ible -> able

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Interactible : MonoBehaviour
+public class Interactable : MonoBehaviour
 {
     public bool isInRange;
     [SerializeField] KeyCode interactKey;
